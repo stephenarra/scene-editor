0733258c2e58056a25da6f9e6f25c5448a7e633b
Stephen Arra <arraworld@gmail.com>
2022-10-03 21:21:46 -0500

    add presence for selected objects

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,47 @@
 import { useStore } from "../utils/useStore";
 
+const getInitials = (str: string) =>
+  str
+    .split(" ")
+    .map((d) => d[0])
+    .join("")
+    .toUpperCase();
+
 const Header = () => {
-  // const name = useStore((state) => state.id);
+  const session = useStore((state) => state.session);
+  const presence = useStore((state) => state.presence);
+  const users = Object.keys(presence).map((id) => ({ ...presence[id], id }));
 
   return (
     <div className="flex items-center justify-between h-full px-4">
       <div>Untitled Scene</div>
-      <div className="avatar placeholder">
-        <div className="w-8 rounded-full bg-neutral-focus text-neutral-content">
-          {/* <span>K</span> */}
+      <div className="flex items-center">
+        <div className="z-50 -space-x-2 overflow-visible avatar-group">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="tooltip tooltip-bottom"
+              data-tip={user.name}
+            >
+              <div className="avatar placeholder">
+                <div
+                  className="w-8 text-neutral-content"
+                  style={{ background: user.color }}
+                >
+                  <span>{getInitials(user.name)}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="border-r-[1px] border-gray-300 mx-3 h-6"></div>
+        <div className="avatar placeholder">
+          <div
+            className="w-8 rounded-full text-neutral-content"
+            style={{ background: session.color }}
+          >
+            <span>{getInitials(session.name)}</span>
+          </div>
         </div>
       </div>
     </div>
